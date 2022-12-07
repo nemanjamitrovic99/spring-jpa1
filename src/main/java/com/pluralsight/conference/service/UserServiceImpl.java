55064e44eb0e5da1fdaa55e2e0dcbd2cb209594c
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-07 16:27:48 +0100

    Service Demo

---
+++ src/main/java/com/pluralsight/conference/service/UserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.pluralsight.conference.service;
+
+import com.pluralsight.conference.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public User save(User user){
+        return null;
+    }
+}
