602c11f1fd420a95a0d531477b9b37d2a431d716
Nemanja Mitrovic <nemanja.mitrovic@en510092.endava.net>
2022-12-09 13:19:34 +0100

    @OneToMany Demo

--- src/main/java/com/pluralsight/conference/model/Registration.java
+++ src/main/java/com/pluralsight/conference/model/Registration.java
@@ -1,10 +1,9 @@
 package com.pluralsight.conference.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "REGISTRATION")
@@ -17,12 +16,23 @@ public class Registration {
     @NotEmpty
     private String name;
 
+    @OneToMany(mappedBy = "registration", cascade = CascadeType.ALL)
+    private List<Course> courses=new ArrayList<>();
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
     }
 
     public String getName() {
